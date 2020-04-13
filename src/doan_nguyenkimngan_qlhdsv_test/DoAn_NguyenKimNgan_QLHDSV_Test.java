d25f36b5ab3755f808af77198381213539ec7d26
dell <dell@dell-PC>
2020-04-13 08:46:40 +0700

    project commit

---
+++ src/doan_nguyenkimngan_qlhdsv_test/DoAn_NguyenKimNgan_QLHDSV_Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doan_nguyenkimngan_qlhdsv_test;
+
+/**
+ *
+ * @author dell
+ */
+public class DoAn_NguyenKimNgan_QLHDSV_Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
